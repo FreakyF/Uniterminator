0aaeabb7003d550795d3451199cb05e31b9d6928
Kamil Fudala <kamil.fudala44@gmail.com>
2025-04-21 19:14:16 +0200

    Fix css and add more form buttons.

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ExpressionArcComponent} from './expression-arc/expression-arc.component';
 import {ExpressionSegmentComponent} from './expression-segment/expression-segment.component';
 
@@ -12,4 +12,25 @@ import {ExpressionSegmentComponent} from './expression-segment/expression-segmen
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // ngOnInit() {
+  //   window.addEventListener('wheel', this.preventZoom, {passive: false});
+  //   window.addEventListener('keydown', this.preventKeyboardZoom);
+  // }
+  //
+  // ngOnDestroy() {
+  //   window.removeEventListener('wheel', this.preventZoom);
+  //   window.removeEventListener('keydown', this.preventKeyboardZoom);
+  // }
+  //
+  // preventZoom = (e: WheelEvent) => {
+  //   if (e.ctrlKey) {
+  //     e.preventDefault();
+  //   }
+  // };
+  //
+  // preventKeyboardZoom = (e: KeyboardEvent) => {
+  //   if (e.ctrlKey && (e.key === '+' || e.key === '-' || e.key === '=')) {
+  //     e.preventDefault();
+  //   }
+  // };
 }
